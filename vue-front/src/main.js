992fb18b77d59b7ad3da7114bc4f22e6029fbfd8
genken <genken.gleb@gmail.com>
2022-03-27 11:01:19 +0300

    main.js lint

--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -1,12 +1,16 @@
+// app init
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+// axios & store init
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
 
 const base = axios.create({
   baseURL: "http://localhost:3000",
@@ -15,10 +19,18 @@ const base = axios.create({
   }
 })
 
-const app = createApp(App)
-  .use(store)
-  .use(router)
-  .use(VueAxios, axios)
-
+app.use(store)
+app.use(VueAxios, axios)
 app.provide('$axios', base)
+
+// bootstrap init
+// import "bootstrap/dist/css/bootstrap.min.css"
+
+
+// app
+//   .use(store)
+//   .use(router)
+//   .use(VueAxios, axios)
+// app.provide('$axios', base)
+
 app.mount('#app')
